d1c07f1c8fa828209efb54249c1e92a594f8cfad
Johnny14012006 <johnnypopescu48@gmail.com>
2024-05-09 21:35:45 +0300

    Add files via upload

---
+++ Site_pentru_atestat-master/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/footer.css'
+
+export default function Footer() {
+  return (
+    <footer>
+        <p style={{textAlign: 'center'}}>Copyright © 2023 Petcu David</p>
+    </footer>
+  )
+}
---
+++ Site_pentru_atestat-master/src/pages/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/contact.css'
+
+export default function Contact() {
+  return (
+    <>
+    <div className="contact-container">
+      <h1>Contact Us</h1>
+      <div className="contact-info">
+        <p>Phone: 0766370884</p>
+        <p>Email: petcudavid2005@gmail.com</p>
+        <p>Instagram: <a href="https://www.instagram.com/petcu_david/">@petcu_david</a></p>
+      </div>
+      <div className='cat'>
+        <img src='/visuals/pisici_minimaliste.jpg' alt='nocat'/>
+      </div>
+    </div>
+    </>
+  )
+}
